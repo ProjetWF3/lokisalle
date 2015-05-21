0bbd3cb1b0cb2e1cbcabb70e07e1580e73d9c2b0
Giordano Jean-Baptiste <jeanbaptiste.giordano@gmail.com>
2015-05-21 15:10:06 +0200

    on a modifier en ajoutant un lien

--- application/theme/footer.php
+++ application/theme/footer.php
@@ -11,6 +11,7 @@
 			</nav>
 		</div>
 	</footer>
+	<a>Modif pour branche</a>|
 		<script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
 		<script src="<?php echo base_url(); ?>assets/js/app.js"></script>
 	</body>
