6c51d32f43cac994259845e4615bcb922e34f04c
Tanala <jeanbaptiste.giordano@gmail.com>
2015-05-25 19:59:40 +0200

    Tanala : J ai presque termine la page recherche lol

--- application/views/recherche.php
+++ application/views/recherche.php
@@ -29,4 +29,31 @@
 	<input type="text" id="pseudo" placeholder="Ex : Paris">
 	<input type="submit" value="">
 </form>
-<div id="resultats">6 résultats</div>
+<div id="resultats-nombre">6 résultats</div>
+<div id="resultats-affichage">
+	
+<?php for ($i=0; $i <6 ; $i++) { 
+
+	
+?>
+	<div class="offres-recherche">
+		<div class="cadre-recherche">
+			<img src="<?php echo base_url(); ?>assets/img/photo.png" width="110" height="110">
+			<div class="infos">
+				<ul class="infos-details">
+					<li>Date : 00 / 00 / 2015</li>
+					<li>Lieu : Paris</li>
+					<li>Prix : 398 $</li>
+					<li>Nbre de personnes : 25</li>
+				</ul>
+				<p> > Fiche detaillée:</p>
+				<div class="panier">
+					<div class="panier-img"><img src="<?php echo base_url(); ?>assets/img/panier.png" alt="panier"></div>
+					<input type="submit" class="ajout-panier" value="Ajouter au panier">
+				</div>
+			</div>
+		</div>
+	</div>
+<?php } ?>
+</div>
+
