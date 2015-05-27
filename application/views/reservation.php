470c694a4d4dd7fcef81e967fd7709c1b30b2190
xaviercav <xcavrois@mail.ru>
2015-05-27 13:36:59 +0200

    xaviercav: google map rajouté

--- application/views/reservation.php
+++ application/views/reservation.php
@@ -62,7 +62,7 @@
 			</ul>
 		</div>
 		<div id="infos-container_droite">
-			<img src="<?php echo base_url(); ?>assets/img/googlemap.png">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.0039510459223!2d2.3669709722713255!3d48.87720122574424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e4d24f9ad17%3A0xa231db7a5a9dcb43!2s24+Rue+Lecluse%2C+75017+Paris!5e0!3m2!1sfr!2sfr!4v1432725906139" width="206" height="206" frameborder="0" style="border:0"></iframe>
 			<a href="">Lien google</a>
 		</div>
 	</div>
