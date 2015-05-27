09adcc5fb7ed6c410dc9a650173aabb6a04fb03c
tomtometnana <contact@e-motionweb.fr>
2015-05-27 13:09:41 +0200

    tomtometnana: debut nouvelle version, page accueil ok

--- application/theme/footer.php
+++ application/theme/footer.php
@@ -1,19 +1,18 @@
-</main>
 	<footer id="footer">
 		<div class="container">
 			<nav id="footer-nav">
 				<ul class="clearfix">
-					<li><a href="<?php echo base_url('page/mentions')?>">Mentions légales</a></li>
-					<li><a href="<?php echo base_url('page/cgv')?>">C.G.V.</a></li>
-					<li><a href="<?php echo base_url('page/plan')?>">Plan du site</a></li>
-					<li><a href="">Imprimer la page</a></li>
-					<li><a href="<?php echo base_url('page/newsletter')?>">S'inscrire à la newsletter</a></li>
-					<li><a href="<?php echo base_url('page/contact'); ?>">Contact</a></li>
+					<li><a href="?page=mentions">Mentions légales</a></li>
+					<li><a href="?page=cgv">C.G.V.</a></li>
+					<li><a href="?page=plan">Plan du site</a></li>
+					<li><a href="#">Imprimer la page</a></li>
+					<li><a href="?page=newsletter">S'inscrire à la newsletter</a></li>
+					<li><a href="?page=contact">Contact</a></li>
 				</ul>
 			</nav>
 		</div>
 	</footer>
-	<?php echo script_tag('assets/js/jquery.js'); ?>
-	<?php echo script_tag('assets/js/app.js'); ?>
+	<?php //echo script_tag('assets/js/jquery.js'); ?>
+	<?php //echo script_tag('assets/js/app.js'); ?>
 	</body>
 </html>
