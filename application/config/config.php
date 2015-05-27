1d0d353c524d3061ee151760b7be5bd35cb74894
tomtometnana <contact@e-motionweb.fr>
2015-05-27 12:29:19 +0200

    tomtomtetnana: config PDO ok

--- application/config/config.php
+++ application/config/config.php
@@ -1,33 +1,56 @@
 <?php
-// chemin de base du site
-define("BASE_URL", "http://localhost/lokisalle/");
-
 //On définit la connection selon le serveur où le script est executé.
 $pathURL = $_SERVER['HTTP_HOST'];
 
 if($pathURL == "127.0.0.1" || $pathURL == "localhost") {
 
-  // Paramètres connexion base locale
-  $host = "localhost"; // adresse
-  $user = "root"; // vide ou "root" en local
-  $pass = ""; // vide ou root(pour les mac) en local
-  $bdd = "lokisalle"; // nom de la BDD
+  define("BASE_URL", "http://localhost/lokisalle/"); // chemin de base du site
 
-  // connexion
-  @mysql_connect($host,$user,$pass) or die('Impossible de se connecter &agrave; '.$host.'<br />'.mysql_error());
-  @mysql_select_db($bdd) or die('Impossible de s&eacute;lectionner la BDD : '.$bdd.'<br />'.mysql_error());
+  define('HOST', 'localhost');
+  define('USER', 'root');
+  define('PASS', '');
+  define('DB', 'lokisalle');
+
+  try {
+
+      global $db;
+
+      $db_options = array(
+          PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      );
+
+      $db = new PDO('mysql:host='.HOST.';dbname='.DB.'', USER, PASS, $db_options);
+
+  } catch (Exception $e) {
+      exit('MySQL Connect Error >> '.$e->getMessage());
+  }
 
 } else { //Infos BDD du site distant (Attention : modifier juste si besoin ;)
 
-  // Paramètres connexion à la basse de donnees
-  $host = ""; // adresse selon hebergeur
-  $user = ""; // login hebergeur
-  $pass = ""; // mot de passe hebergeur
-  $bdd = ""; // nom de la BD selon hebergeur
+  define("BASE_URL", "http://lenomdemonsite.fr"); // chemin de base du site
 
-  // connexion
-  @mysql_connect($host,$user,$pass) or die('Impossible de se connecter &agrave; '.$host.'<br />'.mysql_error());
-  @mysql_select_db($bdd) or die('Impossible de s&eacute;lectionner la BDD : '.$bdd.'<br />'.mysql_error());
+  define('HOST', '');
+  define('USER', '');
+  define('PASS', '');
+  define('DB', '');
+
+  try {
+
+      global $db;
+
+      $db_options = array(
+          PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      );
+
+      $db = new PDO('mysql:host='.HOST.';dbname='.DB.'', USER, PASS, $db_options);
+
+  } catch (Exception $e) {
+      exit('MySQL Connect Error >> '.$e->getMessage());
+  }
 
 }
 ?>
