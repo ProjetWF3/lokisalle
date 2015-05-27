85f8d41132f3591742075e7ccf32ab12d2917f65
tomtometnana <contact@e-motionweb.fr>
2015-05-27 11:25:53 +0200

    tomtometnana: version php procedural en cours

--- application/theme/footer.php
+++ application/theme/footer.php
@@ -3,17 +3,17 @@
 		<div class="container">
 			<nav id="footer-nav">
 				<ul class="clearfix">
-					<li><a href="<?php echo base_url('page/mentions')?>">Mentions légales</a></li>
+					<li><a href="">Mentions légales</a></li>
 					<li><a href="">C.G.V.</a></li>
 					<li><a href="">Plan du site</a></li>
 					<li><a href="">Imprimer la page</a></li>
 					<li><a href="">S'inscrire à la newsletter</a></li>
-					<li><a href="<?php echo base_url('page/contact'); ?>">Contact</a></li>
+					<li><a href="">Contact</a></li>
 				</ul>
 			</nav>
 		</div>
 	</footer>
-	<?php echo script_tag('assets/js/jquery.js'); ?>
-	<?php echo script_tag('assets/js/app.js'); ?>
+	<?php //echo script_tag('assets/js/jquery.js'); ?>
+	<?php //echo script_tag('assets/js/app.js'); ?>
 	</body>
 </html>
